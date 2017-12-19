b44dd59b3b1ddd0a13f93e4675a1f8ba01756df3
Chris <mathematicschris@gmail.com>
2017-12-19 16:41:24 +0100

    Initial commit

---
+++ Data/Test.m
@@ -0,0 +1,59 @@
+function Test
+
+disp('errTotal7.000e-06.mat')
+load('errTotal7.000e-06.mat')
+PT = errorVecMat
+sigma = sqrt(PT*(1-PT)/(10^8))
+
+disp('errTotal7.500e-06.mat')
+load('errTotal7.500e-06.mat')
+PT = errorVecMat
+sigma = sqrt(PT*(1-PT)/(10^8))
+
+
+disp('errTotal8.000e-06.mat')
+load('errTotal8.000e-06.mat')
+PT = errorVecMat
+sigma = sqrt(PT*(1-PT)/(10^8))
+
+disp('errTotal8.500e-06.mat')
+load('errTotal8.500e-06.mat')
+PT = errorVecMat
+sigma = sqrt(PT*(1-PT)/(10^8))
+
+disp('errTotal9.000e-06.mat')
+load('errTotal9.000e-06.mat')
+PT = errorVecMat
+sigma = sqrt(PT*(1-PT)/(10^8))
+
+disp('errTotal9.500e-06.mat')
+load('errTotal9.500e-06.mat')
+PT = errorVecMat
+sigma = sqrt(PT*(1-PT)/(10^8))
+
+disp('errTotal1.000e-05.mat')
+load('errTotal1.000e-05.mat')
+PT = errorVecMat
+sigma = sqrt(PT*(1-PT)/(10^8))
+
+disp('errTotal1.500e-05.mat')
+load('errTotal1.500e-05.mat')
+PT = errorVecMat
+sigma = sqrt(PT*(1-PT)/(10^8))
+
+disp('errTotal2.000e-05.mat')
+load('errTotal2.000e-05.mat')
+PT = errorVecMat
+sigma = sqrt(PT*(1-PT)/(10^8))
+
+disp('errTotal2.500e-05.mat')
+load('errTotal2.500e-05.mat')
+PT = errorVecMat
+sigma = sqrt(PT*(1-PT)/(10^8))
+
+disp('errTotal3.000e-05.mat')
+load('errTotal3.000e-05.mat')
+PT = errorVecMat
+sigma = sqrt(PT*(1-PT)/(10^8))
+
+end
